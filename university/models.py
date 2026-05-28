e1599302f8801a1fea6cc0d3a6a8683ec6e01256
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-05-28 11:47:19 +0400

    !

--- university/models.py
+++ university/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Teacher(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="teacher_profile"
+    )
+
+    specialization = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Subject(models.Model):
+
+    title = models.CharField(max_length=100)
+
+    teacher = models.ForeignKey(
+        Teacher,
+        on_delete=models.CASCADE,
+        related_name="subjects"
+    )
+
+    def __str__(self):
+        return self.title
+    
+
+class Student(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="student_profile"
+    )
+
+    subjects = models.ManyToManyField(
+        Subject,
+        related_name="students"
+    )
+
+    def __str__(self):
+        return self.user.username
+    
+
+
+    # РАЗОБРАТЬ КОД!!!!!!!!!!!!!!!
