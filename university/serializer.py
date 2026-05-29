9b07986bd1ffda64178bd3018af93088d285bfbf
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-05-29 11:44:14 +0400

    !

--- university/serializer.py
+++ university/serializer.py
@@ -12,3 +12,15 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject     # говорим какую модель сериализируем
         fields = "__all__"  # включить все поля модели
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student
+        fields = "__all__"
+
+class TeacherSerializer(serializers.ModelSerializer):
+
+    class Mets:
+        model = Student
+        fields = "__all__"
--- university/views.py
+++ university/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Teacher, Student, Subject
+from .serializer import TeacherSerializer, StudentSerializer, SubjectSerializer
+
+class TeacherViewSet(viewsets.ModelViewSet):
+
+    queryset = Teacher.object.all()
+    serializer_class = TeacherSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+
+    queryset = Student.object.all()
+    serializer_class = StudentSerializer
+
+class subjectViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Subject.object.all()
+    serializer_class = SubjectSerializer
+
