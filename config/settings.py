22ec4cc436e925db61bdbe3cc29daf3c67176b84
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-01 10:16:52 +0400

    jwt

--- config/settings.py
+++ config/settings.py
@@ -131,9 +131,9 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ],
 
-    # Тип авторизации, обычная django session auth
+    # Тип авторизации - Json web token - JWT
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentification.JWTAuthentification",
     ],
 }
 
--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path("api/", include("university.urls")),
+
+    # /api/token/   ->  login
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair"
+    ),
+
+    # /api/token/refresh/   ->  обновить token
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
 ]
