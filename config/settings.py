191b4f74b73e0810ebd2fcc6c9d1adcace046147
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-02 11:39:20 +0400

    postgre db

--- config/settings.py
+++ config/settings.py
@@ -83,9 +83,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+    "default": {
+
+        "ENGINE": "django.db.backends.postgresql",
+
+        "NAME": os.getenv("DB_NAME"),
+
+        "USER": os.getenv("DB_USER"),
+
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+
+        "HOST": os.getenv("DB_HOST"),
+
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
@@ -166,13 +177,9 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "users.User"
 
 CACHES = {
-    
     "default": {
-        
-        "BACKEND":
-            "django.core.cache.backends.locmem.LocMemCache",
-
-        "LOCATION": "unique-snowflake",
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique",
     }
 }
 
