fd434686df67f860b74d48171f53406d151e12fd
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-02 10:23:28 +0400

    pagination

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,13 @@ REST_FRAMEWORK = {
     ],
 
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
+    # Pagination
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.PageNumberPagination",
+
+    # API будет отдавать 2 объекта на страницу
+    "PAGE_SIZE": 2,
 }
 
 AUTH_USER_MODEL = "users.User"
