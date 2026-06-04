380565c6b6e8cb1c9e74dbd63ecd3b6d72363d52
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-04 11:45:51 +0400

    testing

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -209,3 +210,12 @@ CACHES = {
             "LOCATION": "unique",
         }
 """
+
+# увеличиваем длительность жизни токена для проекта в учебных целях
+SIMPLE_JWT = {
+
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+
+}
