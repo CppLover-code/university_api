f8bb7ad0e04c8fd896461c0718fd9e87d29e42f9
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-09 12:29:16 +0400

    !

--- config/settings.py
+++ config/settings.py
@@ -225,5 +225,14 @@ SIMPLE_JWT = {
 CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
+# настройка email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 
--- university/tasks.py
+++ university/tasks.py
@@ -1,10 +1,20 @@
 from celery import shared_task
+from django.core.mail import send_mail
 
 @shared_task
-def send_welcome_email(username):
+def send_welcome_email(email, username):
 
-    print(
-        f"Welcome email sent to {username}"
+    send_mail(
+        subject="Добро пожаловать!",
+        message=(
+            f"Здравствуйте, {username}!\n\n"
+            "Вы успешно зарегистрированы "
+            "в системе University API.\n\n"
+            "Желаем успехов в обучении!"
+        ),
+        from_email=None,
+        recipient_list=[email],
+        fail_silently=False,
     )
 
     return f"Email sent to {username}"
